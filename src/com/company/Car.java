c38c418888412f2313f8d56d80ed1339ef474807
fryger <fryg3r@gmail.com>
2020-06-16 22:41:36 +0200

    v1.1 - Car properties

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,4 +1,27 @@
 package com.company;
 
 public class Car {
+
+    //properties
+    public String brand;
+    public String color;
+    public String segment;
+    public Integer mileage;
+    public Double value;
+
+    //parts
+    private Double brakes;
+    private Double suspension;
+    private Double engine;
+    private Double body;
+    private Double transmission;
+
+    public Car( String brand, String color, String segment, Integer mileage, Double value){
+        this.brand = brand;
+        this.color = color;
+        this.segment = segment;
+        this.mileage = mileage;
+        this.value = value;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,8 @@ public class Main {
         System.out.println(me.CASH);
         System.out.println(me.business.name);
 
-
-
+        Car Brera = new Car("Alfa","Red","premium",10000,10.0);
+        System.out.println(Brera.brand);
 
 
 
